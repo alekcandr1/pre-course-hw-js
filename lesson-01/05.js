53108cf6c39633596cfd83dbb7c54175d751dba2
alekcandr1 <alekcandrmain@gmail.com>
2023-11-15 17:01:30 +0300

    edit 1-5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -9,9 +9,7 @@ function areYouProgrammer(x) {
             console.log('У меня 100 пудов все получится');
             break;
 
-        default: console.log('50 на 50')
-        break;
-
+        default: console.log('50 на 50');
     }
 }
 areYouProgrammer(rule);
