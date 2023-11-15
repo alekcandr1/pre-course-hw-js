5acc11b5cbe10381c4496039105429d2d7e72492
alekcandr1 <alekcandrmain@gmail.com>
2023-11-15 15:54:10 +0300

    del comments

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,6 +1,3 @@
 // code
 let number1 = Math.floor(Math.random() * 100);
-
 console.log(number1 % 2 ? 'Это нечетное число' : 'Это четное число');
-
-// number1 % 2 ? console.log('Это нечетное число') : console.log('Это четное число')
