ba01d7220d18a9c9d1a9185cb856ba962c994be2
alekcandr1 <alekcandrmain@gmail.com>
2023-11-15 15:50:12 +0300

    sprint1-2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,6 @@
 // code
+let number1 = Math.floor(Math.random() * 100);
+
+console.log(number1 % 2 ? 'Это нечетное число' : 'Это четное число');
+
+// number1 % 2 ? console.log('Это нечетное число') : console.log('Это четное число')
