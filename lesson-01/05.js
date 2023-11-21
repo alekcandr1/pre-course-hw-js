8f8371f4f3aa8982ade9462252e2e4246f94e129
alekcandr1 <alekcandrmain@gmail.com>
2023-11-21 15:29:02 +0300

    sprint 2-5

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -14,20 +14,3 @@ switch (true) {
         console.log("А что так можно было?");
         break;
 }
-
-rule = 'программистом';
-
-switch (true) {
-    case rule.length < 25:
-        console.log("Все таки нет правил без исключения");
-        break;
-    case rule.length > 25:
-        console.log("У меня 100 пудов все получится");
-        break;
-    case rule.length === 25:
-        console.log("50 на 50");
-        break;
-    default:
-        console.log("А что так можно было?");
-        break;
-}
