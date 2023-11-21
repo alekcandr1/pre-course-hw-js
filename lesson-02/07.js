7cb92f9a8e68ce22e5a70bcaace0da23b8da3f58
alekcandr1 <alekcandrmain@gmail.com>
2023-11-21 14:50:19 +0300

    sprint 2-7

--- lesson-02/07.js
+++ lesson-02/07.js
@@ -7,6 +7,6 @@ let passportMarried = {
     }
 };
 
-let passportMarried2 = { ...passportMarried, married: true, address: { ...passportMarried.address } };
+let passportMarried2 = { ...passportMarried, married: true };
 console.log(passportMarried);
 console.log(passportMarried2);
